08ae597cd4c1dba661d8112f5b9e72be15d5b256
DARSHAN_M_RD <darshanmrd17@gmail.com>
2024-03-23 13:25:18 +0530

    App.jsx has Components.

---
+++ main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import './style.css'
+import './plugin.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
